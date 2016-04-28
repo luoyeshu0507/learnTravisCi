64191566148f89a9616830466917869b6b4bc0aa
lizhiwei <550822844@qq.com>
2016-04-28 16:55:45 +0800

    add error sass rule

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp=require("gulp");
 var sass=require("gulp-sass");
 gulp.task("default",function(){
 	return gulp.src("front/sass/*.scss")
-			.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+			.pipe(sass({outputStyle: 'compact'}))
 			.pipe(gulp.dest("front/css"))
 });
